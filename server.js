d21c0163df319ca1c6b777a3e9b142b0cbe56b30
dan <danielornelas01@yahoo.com>
2022-06-10 20:24:39 -0700

    working port

--- server.js
+++ server.js
@@ -1,53 +1,22 @@
-const PORT = process.env.PORT || 3001;
-const fs = require('fs');
-const path = require('path');
+// Dependencies
+const express = require("express");
 
-const express = require('express');
+// Express configuration
+//Tells node that we are creating an 'express' server
 const app = express();
 
-const allNotes = require('./db/db.json');
+// Sets an initial port.
+const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({extended: true }));
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('api/notes', (req, res) => {
-    res.json(allNotes.slice(1));
+// ROUTES
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: ${PORT}`);
 });
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-})
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-function createNewNote(body, notesArray) {
-    const newNote = body;
-    if(!Array.isArray(notesArray))
-    notesArray = [];
-
-    if (notesArray.length === 0)
-    notesArray.push(0);
-
-    body.id = notesArray[0];
-    notesArray[0]++;
-
-    notesArray.push(newNote);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesArray, null, 2)
-    );
-    return newNote;
-}
-
-app.post('/api/notes', (req, res) => {
-    const newNote = createNewNote(req.body, allNotes);
-    res.json(newNote);
-});
-
